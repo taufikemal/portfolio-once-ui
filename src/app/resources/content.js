4bf929c1ca5fd05aa02b0f728e06544af113fadf
taufikemal <artcrev7@gmail.com>
2024-09-26 16:29:22 +0700

    change image

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
     },
     role:      'UI/UX Designer',
     avatar:    '/images/avatar.jpg',
-    location:  'Asia/Yogyakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
 }
 
